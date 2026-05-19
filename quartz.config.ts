d2c7fc220e4aef99d37da783bb3caf3f4d7915e2
wajun <102910015+9highfive7@users.noreply.github.com>
2026-05-19 23:38:29 +0900

    Quartz sync: May 19, 2026, 11:38 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "9highfive7.github.io/tech-notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
