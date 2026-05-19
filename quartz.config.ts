f534e3da11c4e277271cda2975bc8d5d3b961ffd
wajun <102910015+9highfive7@users.noreply.github.com>
2026-05-19 23:24:30 +0900

    Quartz sync: May 19, 2026, 11:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "わじゅんの活動記録",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
